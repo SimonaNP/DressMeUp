5202016c3209cbcea7b90550f3e68624165f1561
Цветомила Георгиева < >
2016-07-09 17:19:36 +0300

    some changes

--- app/src/main/java/com/qkikakiood/dressmeup/MainActivity.java
+++ app/src/main/java/com/qkikakiood/dressmeup/MainActivity.java
@@ -1,4 +1,4 @@
- package com.qkikakiood.dressmeup;
+package com.qkikakiood.dressmeup;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -12,42 +12,14 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnTakePhoto;
-    ImageView ingTakenPhoto;
-    private static final int CAM_REQUEST=1313;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnTakePhoto = (Button) findViewById(R.id.btn_add);
-        ingTakenPhoto = (ImageView) findViewById(R.id.img_picture_taken);
-
-        btnTakePhoto.setOnClickListener(new btnTakePhotoClicker());
-
-    }
-@Override
-  protected void onActivityResult(int requestCode, int resultCode, Intent data){
-      super.onActivityResult(requestCode, resultCode, data);
-
-    if(requestCode == CAM_REQUEST){
-        Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
-        ingTakenPhoto.setImageBitmap(thumbnail);
-    }
-  }
-
-    class btnTakePhotoClicker implements Button.OnClickListener{
-
-        @Override
-        public void onClick(View v) {
-            Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            startActivityForResult(cameraIntent,CAM_REQUEST);
-        }
     }
 
-    public void openDressMeActivity(View view) {
-        Intent intent = new Intent(this, DressMe.class);
-    }
 
 }
--- app/src/main/java/com/qkikakiood/dressmeup/SaveImage.java
+++ app/src/main/java/com/qkikakiood/dressmeup/SaveImage.java
@@ -1,17 +1,28 @@
 package com.qkikakiood.dressmeup;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Spinner;
 
 public class SaveImage extends AppCompatActivity {
 
     private String[] spinnerCategory;
+    private String[] spinnerColor;
+
+    Button btnTakePhoto;
+    Button btnDressMe;
+    ImageView imgTakenPhoto;
+    private static final int CAM_REQUEST=1313;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +32,20 @@ public class SaveImage extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         this.spinnerCategory = new String[] {
-        "PANTS", "SHORTS", "SKIRT", "DRESS", "T-SHIRT", "SHIRT", "THANK TOP","LONG-SLEEVE"
+                "PANTS", "SHORTS", "SKIRT", "DRESS", "T-SHIRT", "SHIRT", "THANK TOP","LONG-SLEEVE"
         };
-        Spinner s = (Spinner) findViewById(R.id.spinner_category);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-        android.R.layout.simple_spinner_item, spinnerCategory);
-        s.setAdapter(adapter);
+        Spinner sCat = (Spinner) findViewById(R.id.spinner_category);
+        ArrayAdapter<String> adapterCat = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, spinnerCategory);
+        sCat.setAdapter(adapterCat);
+
+        this.spinnerColor = new String[] {
+                "BLACK", "WHITE", "BLUE", "GREEN", "RED", "PINK", "YELLOW","ORANGE","PURPLE","GREY","BROWN","NUDE"
+        };
+        Spinner sCol = (Spinner) findViewById(R.id.spinner_color);
+        ArrayAdapter<String> adapterCol = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, spinnerColor);
+        sCol.setAdapter(adapterCol);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -36,6 +55,36 @@ public class SaveImage extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        btnTakePhoto = (Button) findViewById(R.id.btn_add);
+        btnDressMe = (Button) findViewById(R.id.btn_dress);
+        imgTakenPhoto = (ImageView) findViewById(R.id.img_picture_taken);
+
+        btnTakePhoto.setOnClickListener(new btnTakePhotoClicker());
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == CAM_REQUEST){
+            Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+            imgTakenPhoto.setImageBitmap(thumbnail);
+        }
+    }
+
+    class btnTakePhotoClicker implements Button.OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+            Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(cameraIntent,CAM_REQUEST);
+
+
+        }
+
     }
 
 }
